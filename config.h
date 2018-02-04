0c4cd28236f0f34c911d11df4324b054187fb44f
E <consolacion@users.noreply.github.com>
2018-02-04 14:08:03 +0100

    Create config.h

---
+++ config.h
@@ -0,0 +1,10 @@
+
+const char* ssid = "";
+const char* password = "";
+#define HOST  "OTA-Weatherstation"
+
+
+//const char* broker = "192.168.1.103";
+const char* mqttClientId = "weather";
+const char* mqttUser = "";
+const char* mqttPwd = "";
